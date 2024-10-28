1b97919fcb16d1143475aa149dbfa62178d3ce2d
Azkalyasin <158534794+Azkalyasin@users.noreply.github.com>
2024-10-28 14:00:05 +0700

    Delete Belajar python/latihan/tempCodeRunnerFile.py

--- Belajar python/latihan/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-    
-n = int(input('masukkan batas bilangan fibonaci: '))
-def fibonaci (n):
-  count = [0, 1] 
-  while len(count) < (n):
-     count.append(count[-2]+count[-1])
-  return count
-print ('fibonaci number are: ',fibonaci(n))
