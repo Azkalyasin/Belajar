1338eb563746a8f9cb3323e8549c8e21c3678f51
Azkalyasin <158534794+Azkalyasin@users.noreply.github.com>
2024-10-28 13:57:21 +0700

    Delete Belajar python/Hari3 directory

--- Belajar python/Hari3/data.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv(r"D:\Data Analys\train.csv")
-
-df.head()
--- Belajar python/Hari3/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("D:\Data Analys\Airbnb_Open_Data.csv")
-
-df.head()
